2696efd8e20bc6364f1afb8fb1a62ec77ce2cbbe
Serhii Sych <sychsergiy@gmail.com>
2020-02-10 21:53:33 +0200

    envrionment setuper context manager added

--- over_provisioning/environment/setuper.py
+++ over_provisioning/environment/setuper.py
@@ -1,6 +1,9 @@
 import typing as t
 
 from over_provisioning.environment.hooks import EnvironmentHook
+from over_provisioning.logger import get_logger
+
+logger = get_logger()
 
 
 class EnvironmentSetuper:
@@ -14,10 +17,33 @@ class EnvironmentSetuper:
     def add_destroy_hook(self, hook: EnvironmentHook):
         self._destroy_hooks.append(hook)
 
-    def create(self):
-        for hook in self._create_hooks:
-            hook.run()
+    def __enter__(self):
+        return self
 
-    def destroy(self):
-        for hook in self._destroy_hooks:
-            hook.run()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.destroy()
+        if exc_type is not None:
+            return False  # reraise exception
+
+    def create(self) -> bool:
+        try:
+            logger.info("Trying to destroy environment")
+            for hook in self._create_hooks:
+                hook.run()
+            logger.info("Environment successfully created")
+            return True
+        except Exception:
+            logger.exception("Failed to setup environment due to the following exception.")
+            return False
+
+    def destroy(self) -> bool:
+        try:
+            logger.info("Trying to destroy environment")
+            for hook in self._destroy_hooks:
+                hook.run()
+            logger.info("Environment successfully destroyed")
+            return True
+        except Exception:
+            logger.exception("Failed to destroy environment due to the following exception.")
+            logger.info("Manual environment cleanup required")
+            return False
